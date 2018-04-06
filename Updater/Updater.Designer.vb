aa67a0358de76be09716ae4ca67074c4544b1b95
Scarala <scarala@gmx.de>
2018-04-06 15:40:15 +0200

    Add files via upload

---
+++ Updater/Updater.Designer.vb
@@ -0,0 +1,91 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Updater
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Updater))
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.ProgressBar1.Location = New System.Drawing.Point(48, 25)
+        Me.ProgressBar1.MarqueeAnimationSpeed = 0
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(356, 23)
+        Me.ProgressBar1.Step = 1
+        Me.ProgressBar1.TabIndex = 0
+        '
+        'Label3
+        '
+        Me.Label3.Location = New System.Drawing.Point(12, 52)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(392, 15)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Label1"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(144, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Update wird heruntergeladen"
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Location = New System.Drawing.Point(12, 25)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 23)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Label2"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Updater
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(416, 77)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Updater"
+        Me.Text = "Hoer-Talk Projektmanager - Autoupdater"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Updater/Updater.vb
@@ -0,0 +1,53 @@
+﻿Imports System.IO
+Imports System.IO.Compression
+
+Public Class Updater
+    Dim url As String = "http://scarala-design.de/projektmanager/Update.zip"
+    Public WithEvents wc As New Net.WebClient
+    Dim Version As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim myFileVersionInfo As FileVersionInfo = FileVersionInfo.GetVersionInfo(System.AppDomain.CurrentDomain.BaseDirectory & "Hörspiel Projektmanager.exe")
+        Version = Replace(myFileVersionInfo.FileVersion, ".", "")
+        Try
+            Dim client As New Net.WebClient
+            Dim update As String = client.DownloadString("http://scarala-design.de/projektmanager/version.txt")
+            If update > Version Then
+                wc.DownloadFileAsync(New Uri(url), ".\Update.zip")
+            Else
+                If MsgBox("Kein Update zu installieren!" & vbNewLine & "Programm starten?", MsgBoxStyle.YesNo, "Kein Update") = vbYes Then
+                    Process.Start(System.AppDomain.CurrentDomain.BaseDirectory & "Hörspiel Projektmanager.exe")
+                    Application.Exit()
+                Else
+                    Application.Exit()
+                End If
+            End If
+        Catch ex As Exception
+            MsgBox("Keine Verbindung zum Server möglich")
+        End Try
+    End Sub
+
+    Private Sub wc_DownloadFileCompleted(ByVal sender As Object, ByVal e As System.ComponentModel.AsyncCompletedEventArgs) Handles wc.DownloadFileCompleted
+        Dim startPath As String = System.AppDomain.CurrentDomain.BaseDirectory
+        Dim zipPath As String = System.AppDomain.CurrentDomain.BaseDirectory & "\Update.zip"
+        Dim extractPath As String = System.AppDomain.CurrentDomain.BaseDirectory
+
+        File.Delete(System.AppDomain.CurrentDomain.BaseDirectory & "\Hörspiel Projektmanager.exe")
+
+        ZipFile.ExtractToDirectory(zipPath, extractPath, System.Text.Encoding.GetEncoding(850))
+
+        File.Delete(System.AppDomain.CurrentDomain.BaseDirectory & "\Update.zip")
+
+        If MsgBox("Update erfolgreich installiert!" & vbNewLine & "Programm starten?", MsgBoxStyle.YesNo, "Erfolgreich!") = vbYes Then
+            Process.Start(System.AppDomain.CurrentDomain.BaseDirectory & "Hörspiel Projektmanager.exe")
+            Application.Exit()
+        Else
+            Application.Exit()
+        End If
+    End Sub
+    Private Sub wc_DownloadProgressChanged(ByVal sender As Object, ByVal e As System.Net.DownloadProgressChangedEventArgs) Handles wc.DownloadProgressChanged
+        Me.ProgressBar1.Value = e.ProgressPercentage
+        Me.Label3.Text = e.BytesReceived / 1024 & " von " & e.TotalBytesToReceive / 1024 & " KB "
+        Me.Label2.Text = e.ProgressPercentage & "%"
+    End Sub
+End Class
